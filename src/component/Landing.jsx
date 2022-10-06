54f6732ef2afcf1c928126f748fa25d245aeb540
Oduyale Enoch <oduyaleenoch@gmail.com>
2022-10-06 12:59:09 +0100

    Update landing.jsx

--- src/component/Landing.jsx
+++ src/component/Landing.jsx
@@ -43,8 +43,7 @@ export const Landing = () => {
         </div>
 
       </div>
-      <div className="absolute bottom-0 right-0   bg-[#053C64] h-[50px] w-[50px] md:h-[200px] md:w-[200px] rounded-tl-[90%] "></div>
-
+      
     </section>
   );
 };
