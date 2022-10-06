287fe0da1045c941461f9066f7322c13d66a53ca
Oduyale Enoch <oduyaleenoch@gmail.com>
2022-10-06 13:00:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ function App() {
   return (
     <>
       <Landing />
+<div className="absolute bottom-0 right-0   bg-[#053C64] h-[50px] w-[50px] md:h-[200px] md:w-[200px] rounded-tl-[90%] "></div>
+
     </>
   );
 }
